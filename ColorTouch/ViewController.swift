bb026e7c4771e711f7f35e350040d28b394afb53
Kseniia Agibalova <agibalovaks@bk.ru>
2022-08-18 00:08:15 +0300

    .......

--- ColorTouch/ViewController.swift
+++ ColorTouch/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    override var prefersStatusBarHidden: Bool
+    {return true}
+var colorOfButton = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        colorOfButton += 1
+        print(colorOfButton)
+        switch colorOfButton{
+            
+        case 1:
+            view.backgroundColor = .red
+        case 2:
+            view.backgroundColor = .orange
+        case 3:
+            view.backgroundColor = .yellow
+        case 4:
+            view.backgroundColor = .green
+        case 5:
+            view.backgroundColor = .cyan
+        case 6:
+            view.backgroundColor = .blue
+        case 7:
+            view.backgroundColor = .purple
+        default: view.backgroundColor = .black
+    }
 
 }
 
+}
